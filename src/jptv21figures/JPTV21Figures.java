62b7ca155389a3823b1bc56acf71ed7df9733785
pupil <pupil@ivkhk.ee>
2022-09-22 12:45:25 +0300

    first

---
+++ src/jptv21figures/JPTV21Figures.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptv21figures;
+
+/**
+ *
+ * @author pupil
+ */
+public class JPTV21Figures {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
